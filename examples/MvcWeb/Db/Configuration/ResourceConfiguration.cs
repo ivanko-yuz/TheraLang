7d50599219330a1f21e1c2077abb2029ec7b0557
Dmitry Moment <dimamomentmds@gmail.com>
2019-10-26 17:25:17 +0300

    Added Tables and Configurations: Dima Moment

---
+++ examples/MvcWeb/Db/Configuration/ResourceConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MvcWeb.Db.Entities;
+
+namespace MvcWeb.Db.Configuration
+{
+    public class ResourceConfiguration : IEntityTypeConfiguration<Resource>
+    {
+        public void Configure(EntityTypeBuilder<Resource> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(500).IsRequired();
+            builder.Property(p => p.Date).IsRequired();
+        }
+    }
+}
---
+++ examples/MvcWeb/Db/Entities/ResourceCategory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MvcWeb.Db.Entities
+{
+    public class ResourceCategory
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public virtual List<Resource> Resources { get; set; }
+        public ResourceCategory()
+        {
+            this.Resources = new List<Resource>();
+        }
+    }
+}
