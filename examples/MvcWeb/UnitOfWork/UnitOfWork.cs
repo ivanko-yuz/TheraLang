2c3e06ca5368a8d938167868af8e87e9d20a01c1
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-10-26 13:53:20 +0300

    added UoW

---
+++ examples/MvcWeb/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcWeb.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext Context { get; }
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+        }
+
+        public IQueryable<TEntity> Queryable<TEntity>() where TEntity : class => Context.Set<TEntity>();
+
+        public Task<int> SaveChangesAsync() => Context.SaveChangesAsync();
+    }
+}
