935967d1a58928dc0b15531355d08be1cf5510b6
Serhii Volchetskyi <49446721+dgzargo@users.noreply.github.com>
2019-10-26 14:05:17 +0300

    Feature/add unit of work (#8)
    * added UoW
    * gode cleanup

---
+++ examples/MvcWeb/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWeb.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IQueryable<TEntity> Queryable<TEntity>() where TEntity : class;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ examples/MvcWeb/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcWeb.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+        }
+
+        private DbContext Context { get; }
+
+        public IQueryable<TEntity> Queryable<TEntity>() where TEntity : class
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return Context.SaveChangesAsync();
+        }
+    }
+}
