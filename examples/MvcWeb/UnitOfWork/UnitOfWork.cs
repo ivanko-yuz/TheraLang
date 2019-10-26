19c1fd633ef34fdaad7c28a6efcb86de7631fc6c
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-10-26 14:02:47 +0300

    gode cleanup

--- examples/MvcWeb/UnitOfWork/UnitOfWork.cs
+++ examples/MvcWeb/UnitOfWork/UnitOfWork.cs
@@ -6,15 +6,21 @@ namespace MvcWeb.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private DbContext Context { get; }
-
         public UnitOfWork(DbContext context)
         {
             Context = context;
         }
 
-        public IQueryable<TEntity> Queryable<TEntity>() where TEntity : class => Context.Set<TEntity>();
+        private DbContext Context { get; }
 
-        public Task<int> SaveChangesAsync() => Context.SaveChangesAsync();
+        public IQueryable<TEntity> Queryable<TEntity>() where TEntity : class
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return Context.SaveChangesAsync();
+        }
     }
 }
