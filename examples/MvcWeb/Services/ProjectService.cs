c08cfa7048ee529c008f7e8f1346a1d84e5eaaa4
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-10-30 16:40:31 +0200

    added ViewModel, its Validator, Service and Controller for Project Entity. registered all dependencies

---
+++ examples/MvcWeb/Services/ProjectService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvcWeb.Models;
+using MvcWeb.TheraLang.Entities;
+using MvcWeb.TheraLang.UnitOfWork;
+
+namespace MvcWeb.Services
+{
+    public interface IProjectService
+    {
+        Task<bool> TryAddProject(ProjectViewModel projectViewModel);
+    }
+    
+    public class ProjectService:IProjectService
+    {
+        private IUnitOfWork UnitOfWork { get; }
+
+        public ProjectService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+        
+        public async Task<bool> TryAddProject(ProjectViewModel projectViewModel)
+        {
+            var newProject = new Project {Name = projectViewModel.Name, Type = projectViewModel.Type};
+            try
+            {
+                await UnitOfWork.Repository<Project>().Add(newProject);
+                await UnitOfWork.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
