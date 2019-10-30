2e47d1a7dd4092f9878913ed934c901b800b62a9
Hrysyuk Arsen <archytrade@gmail.com>
2019-10-30 13:53:53 +0200

    Added create project components and  button

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule,MatButtonModule,MatCardModule } from '@angular/material';
+import { MatToolbarModule,MatButtonModule,MatCardModule, MatDialogModule, MatIconModule, MatFormField } from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule, routingComponents  } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,11 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { ProjectComponent } from './project/project.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectInfoComponent } from './project-info/project-info.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
+import { ReactiveFormsModule,FormsModule } from "@angular/forms";
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -17,8 +22,10 @@ import { ProjectInfoComponent } from './project-info/project-info.component';
     ToolbarComponent,
     ProjectComponent,
     HomeComponent,
-    ProjectInfoComponent
+    ProjectInfoComponent,
+    CreateProjectComponent,
   ],
+  entryComponents:[CreateProjectComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,7 +33,13 @@ import { ProjectInfoComponent } from './project-info/project-info.component';
     MatToolbarModule,
     MatButtonModule,
     HttpClientModule,
-    MatCardModule
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
