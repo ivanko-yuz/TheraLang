506ee63aad5153a98370320c6ed6f577318c1c10
Dmitry Moment <dimamomentmds@gmail.com>
2019-11-03 15:26:24 +0200

    baseEntity, resource controller

---
+++ examples/MvcWeb/TheraLang/Configuration/ResourceCategoriesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MvcWeb.TheraLang.Entities;
+
+namespace MvcWeb.TheraLang.Configuration
+{
+    public class ResourceCategoriesConfiguration : IEntityTypeConfiguration<ResourceCategory>
+    {
+        public void Configure(EntityTypeBuilder<ResourceCategory> builder)
+        {
+            builder.ToTable("ResourceCategories");
+
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Id).ValueGeneratedOnAdd();
+            builder.Property(i => i.Type).HasMaxLength(20).IsRequired();
+
+            // Relationship.
+            builder.HasMany(c => c.Resources).WithOne(r => r.ResourceCategory).HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
