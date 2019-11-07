e81d9a98965e1b7969d312cc477c3dc157650bcd
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-11-07 22:36:24 +0200

    added project heraLang.DataSeeding

---
+++ examples/TheraLang.DataSeeding/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MvcWeb.Db;
+using MvcWeb.TheraLang.Entities;
+
+namespace TheraLang.DataSeeding
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var dbContext = CreateDbContext();
+
+            dbContext.ClearAndSeed(Projects());
+        }
+
+        static IEnumerable<Project> Projects()
+        {
+            yield return new Project
+            {
+                Name = "ProjectName",
+                Type = "ProjectType",
+            };
+            yield return new Project
+            {
+                Name = "ProjectName",
+                Type = "ProjectType",
+            };
+        }
+        
+        static DbContext CreateDbContext()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            var configuration = configurationBuilder.Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder()
+                .UseSqlServer(connectionString);
+            var dbContextOptions = dbContextOptionsBuilder.Options;
+
+            return new IttmmDbContext(dbContextOptions);
+        }
+    }
+}
