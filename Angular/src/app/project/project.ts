909e892441e118e23391ba7db8c2a8768a8d5c28
Pavel Rishko <rishkomail.ua@gmail.com>
2019-11-08 23:32:19 +0200

    Resources table (#45)
    * fixes
    
    * fix button func
    
    * added ViewModel, its Validator, Service and Controller for Project Entity. registered all dependencies (#18)
    
    * Jira task uttmm 27 (#19)
    
    * added ViewModel, its Validator, Service and Controller for Project Entity. registered all dependencies
    
    * ProjectService code cleanup
    
    * fix proj model in mvcMock
    
    * fixed project description by api
    
    * angularTable
    
    * i break everything
    
    * qw
    
    * added new modules
    
    * Fixed table & ApiMock
    
    * added pagination, unsubscribe for http, added new methods for table
    
    * Fix sorting & resource showing
    
    * Refactoring
    
    * Added Resources page
    
    * fixea common resTable
    
    * Fixed common Resources button & res controller
    
    * Del .idea(Webstorm ginfigs) folder
    
    * Remove node modules folder
    
    * Fix
    
    * deleted unused code & fixed namings
    
    * Fixed paginator

--- Angular/src/app/project/project.ts
+++ Angular/src/app/project/project.ts
@@ -1,7 +1,8 @@
 export class Project {
     constructor(
-        public id:number,
+        public id: number,
         public name: string,
+        public description: string,
         public type: string
         ) { }
 }
