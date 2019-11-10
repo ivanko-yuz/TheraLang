3a38894fde12ba66ff7e71924a2e896ce78224ab
Pavlo Rishko <markliveplus@gmail.com>
2019-11-10 13:56:12 +0200

    added component pages-menu-button

---
+++ Angular/src/app/pages-menu-button/pages-menu-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-menu-button',
+  templateUrl: './pages-menu-button.component.html',
+  styleUrls: ['./pages-menu-button.component.less']
+})
+export class PagesMenuButtonComponent implements OnInit {
+
+  constructor() { }
+  teasers: any;
+  ngOnInit() {
+  }
+
+}
