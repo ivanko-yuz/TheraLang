79043dc2cf3988edb4732d2078ff0ac0a8075041
Pavlo Rishko <markliveplus@gmail.com>
2019-11-10 18:25:47 +0200

    added new http request

--- Angular/src/app/project/shared/http-project.service.ts
+++ Angular/src/app/project/shared/http-project.service.ts
@@ -11,6 +11,7 @@ export class HttpProjectService {
   constructor(private http: HttpClient) { }
 
   private url = "https://localhost:44353/api/project";
+  private piranhaApiUrl = "http://localhost:5000/api";
     
     getAllProjects(){
         return this.http.get(this.url);
@@ -18,19 +19,18 @@ export class HttpProjectService {
     getProjectInfo(id:number){      
         return this.http.get(this.url + '/' + id);       
     }
-
     getAllResourcesById(projectId : number){
-        return this.http.get(this.url + '/' + projectId + '/' + 'resources');
-    }
-    getAllResourcesWithoutId( ){
-        return this.http.get('resources');
+        return this.http.get(this.url + '/' + projectId + '/resources');
     }
     createProject(project:Project) {
         return this.http.post(this.url, project);
     }
-
     updateProject(project: Project) {
   
         return this.http.put(this.url + '/' + project.id, project);
+    }
+
+    getSiteMap(){
+        return this.http.get(this.url + '/sitemap')
     }
 }
