deb94798974e6b86b66909f668dbc33383283930
AlexBoichuk <43930744+AlexBoichuk@users.noreply.github.com>
2019-11-12 17:27:52 +0200

    Uttmm 34 (#40)
    * added requests
    
    * added icon notification
    
    * fixed noification button updates sync with requests
    
    * changed names of some variables to more appropriate
    
    * added enum const
    
    * changed some naming
    
    * renamed enum
    
    * deleted unused variable

---
+++ Angular/src/app/project/project.ts
@@ -0,0 +1,11 @@
+export class Project {
+    constructor(
+        public id: number,
+        public name: string,
+        public description : string,
+        public details : string,
+        public projectBegin : Date,
+        public projectEnd  : Date,
+        public isActive  : boolean
+        ) { }
+}
