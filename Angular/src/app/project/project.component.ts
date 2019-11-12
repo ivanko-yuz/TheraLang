e496c284d89a937bcc9bd6d4e8dc845c7a17e0f2
AlexBoichuk <43930744+AlexBoichuk@users.noreply.github.com>
2019-11-12 17:27:52 +0200

    Uttmm 34 (#40)
    * added requests
    
    * added icon notification
    
    * fixed noification button updates sync with requests
    
    * changed names of some variables to more appropriate
    
    * added enum const
    
    * changed some naming
    
    * renamed enum
    
    * deleted unused variable

--- Angular/src/app/project/project.component.ts
+++ Angular/src/app/project/project.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpService} from './http.service';
-import {Project} from './project';
-import {MatDialog, MatDialogConfig} from '@angular/material';
+import { HttpService } from './http.service';
+import { Project } from './project';
+import { MatDialog, MatDialogConfig } from '@angular/material';
 import { CreateProjectComponent } from '../create-project/create-project.component';
 import { Resource } from '../resources-table/resource';
 
@@ -9,25 +9,24 @@ import { Resource } from '../resources-table/resource';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.less'],
-  providers: [HttpService]
+  styleUrls: ['./project.component.less']
 })
 export class ProjectComponent implements OnInit {
 
   projects: Project[];
 
   constructor(private httpService: HttpService,
-              private dialog: MatDialog) { }
+    private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.httpService.getAllProjects().subscribe((data: Project[]) => this.projects = data);
+    this.httpService.getAllProjects().subscribe((projects: Project[]) => this.projects = projects);
   }
 
   onCreate() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
-    dialogConfig.width = '60%';
-    dialogConfig.height = '40%';
+    dialogConfig.width = "60%";
+    dialogConfig.height = "40%";
     this.dialog.open(CreateProjectComponent, dialogConfig);
   }
 
