2a72c20f396ede66d4602256068dcb4d548c4188
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-11-12 18:42:10 +0200

    backend rebased onto WebApi instead of RaisorPages; added PiranhaSitemapService

--- examples/MvcWeb/Controllers/CmsController.cs
+++ examples/MvcWeb/Controllers/CmsController.cs
@@ -2,38 +2,35 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MvcWeb.Services;
 using Piranha;
 using Piranha.AspNetCore.Services;
 using Piranha.Models;
 
 namespace MvcWeb.Controllers
 {
-    public class CmsController : Controller
+    [Route("/api/[controller]")]
+    public class CmsController : ControllerBase
     {
+        private readonly PiranhaSitemapService _piranhaSitemapService;
         private readonly IApi _api;
         private readonly IDb _db;
         private readonly IModelLoader _loader;
 
-        /// <summary>
-        /// Default constructor.
-        /// </summary>
-        /// <param name="app">The current app</param>
-        public CmsController(IApi api, IDb db, IModelLoader loader)
+        public CmsController(IApi api, IDb db, IModelLoader loader, PiranhaSitemapService piranhaSitemapService)
         {
+            _piranhaSitemapService = piranhaSitemapService;
             _api = api;
             _db = db;
             _loader = loader;
         }
 
-        /// <summary>
-        /// Gets the blog archive with the given id.
-        /// </summary>
-        /// <param name="id">The unique page id</param>
-        /// <param name="year">The optional year</param>
-        /// <param name="month">The optional month</param>
-        /// <param name="page">The optional page</param>
-        /// <param name="category">The optional category</param>
-        /// <param name="tag">The optional tag</param>
+        [HttpGet("sitemap")]
+        public async Task<IActionResult> Sitemap(Guid? id = null)
+        {
+            return Ok(await _piranhaSitemapService.GetSiteMap());
+        }
+
         [Route("archive")]
         public async Task<IActionResult> Archive(Guid id, int? year = null, int? month = null, int? page = null,
             Guid? category = null, Guid? tag = null)
@@ -41,51 +38,33 @@ namespace MvcWeb.Controllers
             var model = await _api.Pages.GetByIdAsync<Models.BlogArchive>(id);
             model.Archive = await _api.Archives.GetByIdAsync(id, page, category, tag, year, month);
 
-            return View(model);
+            return Ok(model);
         }
 
-        /// <summary>
-        /// Gets the page with the given id.
-        /// </summary>
-        /// <param name="id">The unique page id</param>
         [Route("page")]
         public async Task<IActionResult> Page(Guid id, bool draft = false)
         {
             var model = await _loader.GetPage<Models.StandardPage>(id, HttpContext.User, draft);
 
-            return View(model);
+            return Ok(model);
         }
 
-        /// <summary>
-        /// Gets the page with the given id.
-        /// </summary>
-        /// <param name="id">The unique page id</param>
         [Route("pagewide")]
         public async Task<IActionResult> PageWide(Guid id, bool draft = false)
         {
             var model = await _loader.GetPage<Models.StandardPage>(id, HttpContext.User, draft);
 
-            return View(model);
+            return Ok(model);
         }
 
-        /// <summary>
-        /// Gets the post with the given id.
-        /// </summary>
-        /// <param name="id">The unique post id</param>
-        ///
         [Route("post")]
         public async Task<IActionResult> Post(Guid id, bool draft = false)
         {
             var model = await _loader.GetPost<Models.BlogPost>(id, HttpContext.User, draft);
 
-            return View(model);
+            return Ok(model);
         }
 
-        /// <summary>
-        /// Gets the teaser page with the given id.
-        /// </summary>
-        /// <param name="id">The page id</param>
-        /// <param name="startpage">If this is the startpage of the site</param>
         [Route("teaserpage")]
         public async Task<IActionResult> TeaserPage(Guid id, bool startpage = false, bool draft = false)
         {
@@ -103,9 +82,9 @@ namespace MvcWeb.Controllers
                     model.LatestPost = await _api.Posts
                         .GetByIdAsync<PostInfo>(latest.First());
                 }
-                return View("startpage", model);
+                return Ok(model);
             }
-            return View(model);
+            return Ok(model);
         }
     }
 }
---
+++ examples/MvcWeb/Services/PiranhaSitemapService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Piranha;
+using Piranha.Models;
+
+namespace MvcWeb.Services
+{
+    public class PiranhaSitemapService
+    {
+        private IApi Api { get; }
+
+        public PiranhaSitemapService(IApi api)
+        {
+            Api = api;
+        }
+        
+        public async Task<Sitemap> GetSiteMap(Guid? id = null)
+        {
+            var siteMap = await Api.Sites.GetSitemapAsync(id);
+
+            foreach (var siteMapItem in siteMap)
+            {
+                await siteMapItem.RecursiveAdd(GetChildSitemapItems);
+            }
+
+            return siteMap;
+        }
+        
+        private async Task<IEnumerable<SitemapItem>> GetChildSitemapItems(SitemapItem sitemapItem)
+        {
+            var items = new List<SitemapItem>();
+            
+             (await GetChildPages(sitemapItem?.Id))
+                .PublishedOnly()
+                .ToSitemap(sitemapItem?.Id, sitemapItem?.Level ?? 0)
+                .AddRangeToList(items);
+                    
+            if (sitemapItem != null)
+            {
+                (await GetChildPosts(sitemapItem.Id))
+                    .PublishedOnly()
+                    .ToSitemap(sitemapItem.Id, sitemapItem.Level)
+                    .AddRangeToList(items);
+            }
+            
+            return items;
+        }
+        
+        private Task<IEnumerable<DynamicPage>> GetChildPages(Guid? parentId)
+        {
+            return Api.Pages.GetAllAsync(parentId);
+        }
+        
+        private Task<IEnumerable<DynamicPost>> GetChildPosts(Guid parentId)
+        {
+            return Api.Posts.GetAllAsync(parentId);
+        }
+    }
+    
+    public static class PiranhaModelHelper
+    {
+        public static IEnumerable<SitemapItem> ToSitemap(this IEnumerable<RoutedContent> enumerable, Guid? parentId, int parentLevel)
+        {
+            var sortOrder = 0;
+            foreach (var item in enumerable)
+            {
+                yield return new SitemapItem
+                {
+                    ParentId = parentId,
+                    SortOrder = sortOrder++,
+                    Title = item.Title,
+                    NavigationTitle = item.Title,
+                    PageTypeName = item.TypeId,
+                    Permalink = item.Permalink,
+                    Published = item.Published,
+                    Created = item.Created,
+                    LastModified = item.LastModified,
+                    Id = item.Id,
+                    Level = parentLevel + 1,
+                };
+            }
+        }
+        
+        public static IEnumerable<RoutedContent> PublishedOnly(this IEnumerable<RoutedContent> content)
+        {
+            return content.Where(p => p.Published <= DateTime.Now);
+        }
+
+        public static void AddRangeToList<TElement>(this IEnumerable<TElement> content, List<TElement> list)
+        {
+            list.AddRange(content);
+        }
+
+        public static async Task RecursiveAdd(this SitemapItem sitemapItem,
+            Func<SitemapItem, Task<IEnumerable<SitemapItem>>> getChildren)
+        {
+            var children = (await getChildren(sitemapItem)).ToArray();
+            foreach (var child in children)
+            {
+                await child.RecursiveAdd(getChildren);
+            }
+            sitemapItem.Items.AddRange(children);
+        }
+    }
+}
