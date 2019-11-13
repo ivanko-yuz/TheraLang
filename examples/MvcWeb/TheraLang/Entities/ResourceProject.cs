780d89228a930c11d9beb192eda74d293bc49347
Pavlo Rishko <markliveplus@gmail.com>
2019-11-13 15:44:53 +0200

    Del .idea files

--- examples/MvcWeb/TheraLang/Entities/ResourceProject.cs
+++ examples/MvcWeb/TheraLang/Entities/ResourceProject.cs
@@ -2,8 +2,7 @@
 {
     public class ResourceProject
     {
-        public int Id { get; set; }
-        // If we use ResourceProjectId is it nessesary to use this foreign key and navigation property ?
+        public int Id { get; set; }       
         public int ResourceId { get; set; }
 
         public virtual Resource Resource { get; set; }
