e25a3cb47d63ba57014f9d5697f0a21b405bf240
stetskivostap <43217586+stetskivostap@users.noreply.github.com>
2019-11-18 20:26:29 +0200

    Update IttmmDbContextModelSnapshot.cs

--- examples/MvcWeb/Migrations/IttmmDbContextModelSnapshot.cs
+++ examples/MvcWeb/Migrations/IttmmDbContextModelSnapshot.cs
@@ -949,8 +949,8 @@ namespace MvcWeb.Migrations
                 });
 
             modelBuilder.Entity("MvcWeb.TheraLang.Entities.ResourceProject", b =>
-
-
+                    {
+                    
                     b.Property<int>("ProjectId");
 
                     b.Property<int>("ResourceId");
