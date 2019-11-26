3dce36db0b2931825dd2c69979893a523cbcf2f5
TarasZadilskyi <56221070+TarasZadilskyi@users.noreply.github.com>
2019-11-26 02:12:28 +0200

    UTTMM-32 Approve/reject project back-end
    * Task32
    
    * U
    
    * UTTMM-32.Add ProjectController.Migration
    
    * merge commit
    
    * All changes made
    
    * done all commits
    
    * change config
    
    * done commits

--- examples/MvcWeb/TheraLang/Configuration/ProjectConfiguration.cs
+++ examples/MvcWeb/TheraLang/Configuration/ProjectConfiguration.cs
@@ -16,8 +16,14 @@ namespace MvcWeb.TheraLang.Configuration
             builder.Property(x => x.Name).HasMaxLength(250).IsRequired();
             builder.Property(x => x.Type).HasMaxLength(250).IsRequired();
 
-            builder.HasMany(x => x.ResourceProjects).WithOne(i => i.Project).HasForeignKey("ProjectId");
+            builder.HasMany(x => x.ProjectResources).WithOne(i => i.Project).HasForeignKey("ProjectId");
             builder.HasMany(x => x.ProjectParticipations).WithOne(i => i.Project).HasForeignKey("ProjectId");
+            builder.Property(e => e.Name).HasMaxLength(250).IsRequired();
+
+            builder.Property(e => e.Type).HasMaxLength(250).IsRequired();
+
+            builder.Property(e => e.StatusId).HasMaxLength(250).IsRequired().HasDefaultValue(Entities.ProjectStatus.New); 
+
         }
     }
 }
