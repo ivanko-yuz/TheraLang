7f8bc8127f7f839f1ff77ba152ae3ea634127994
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-12-13 17:02:35 +0200

    added i18n for project form

--- TheraLang.Web/ClientApp/src/app/toolbar/language/language.component.ts
+++ TheraLang.Web/ClientApp/src/app/toolbar/language/language.component.ts
@@ -9,7 +9,7 @@ import {TranslateService} from '@ngx-translate/core';
 export class LanguageComponent implements OnInit {
 
   @ViewChild('menu', {static: false}) menu: any;
-  languages = ['en', 'ua'];
+  languages = ['en', 'uk'];
 
   constructor(private translate: TranslateService) { }
 
