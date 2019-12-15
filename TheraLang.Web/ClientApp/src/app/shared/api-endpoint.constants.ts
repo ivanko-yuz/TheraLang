158c2a0353d9a772fcaf1acb11e69273f03e2471
Oleksandr Boichuk <boichukedcdsg@gmail.com>
2019-12-15 19:39:37 +0200

    Changed default url

--- TheraLang.Web/ClientApp/src/app/shared/api-endpoint.constants.ts
+++ TheraLang.Web/ClientApp/src/app/shared/api-endpoint.constants.ts
@@ -1,5 +1,5 @@
 export const liqpayCheckoutUrl = "https://www.liqpay.ua/api/3/checkout";
-export const baseUrl = "http://localhost:5000/api/";
+export const baseUrl = "/api/";
 
 export const projectUrl = baseUrl + "projects";
 export const resourсeUrl = baseUrl + "resources";
@@ -7,6 +7,6 @@ export const requestUrl = baseUrl + "request";
 export const categoryUrl = baseUrl + "categories";
 export const participationUrl = baseUrl + "participants";
 export const donationUrl = baseUrl + "donations";
-export const projectType = baseUrl + "projectTypes";
+export const projectTypeUrl = baseUrl + "projectTypes";
 export const cmsSitemapUrl = baseUrl + "sitemap";
 export const accountUrl = baseUrl + "account";
