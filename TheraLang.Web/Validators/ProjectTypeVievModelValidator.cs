69d3efd83f06ffacf33809a490c44e825b4019de
stetskivostap@gmail.com <stetskivostap@gmail.com>
2020-01-03 11:47:17 +0200

    added ProjectTypeModel, and ProjectType Validator

---
+++ TheraLang.Web/Validators/ProjectTypeVievModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Linq.Expressions;
+using TheraLang.DLL.Models;
+
+namespace TheraLang.Web.Validators
+{
+    public class ProjectTypeVievModelValidator :  AbstractValidator<ProjectTypeModel>
+    {
+        private static string ValidateStringLengthMessage =>
+        "The field {PropertyName} has to be less than {MinLength} and more than {MaxLength}. Current length is {TotalLength}";
+        public ProjectTypeVievModelValidator()
+        {
+            ValidateStringLength(p => p.TypeName, 3, 40);            
+        }
+
+        private void ValidateStringLength(Expression<Func<ProjectTypeModel, string>> expression, int minLength, int maxLength)
+        {
+            RuleFor(expression)
+                .NotEmpty()
+                .Length(minLength, maxLength)
+                .WithMessage(ValidateStringLengthMessage);
+        }
+    }
+}
