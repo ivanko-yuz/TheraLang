9d4601c337584447e0bf58e30d5ec949e95e4858
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-01-25 00:11:24 +0200

    Update ResourceViewModel.cs

--- TheraLang.Web/ViewModels/ResourceViewModel.cs
+++ TheraLang.Web/ViewModels/ResourceViewModel.cs
@@ -5,25 +5,18 @@ namespace TheraLang.Web.ViewModels
 {
     public class ResourceViewModel
     {
-        [JsonProperty("id")]
         public int Id { get; set; }
         
-        [JsonProperty("name")]
         public string Name { get; set; }
 
-        [JsonProperty("description")]
         public string Description { get; set; }
 
-        [JsonProperty("url")]
         public string Url { get; set; }
 
-        [JsonProperty("fileName")]
         public string FileName { get; set; }
-
-        [JsonProperty("file")]
+        
         public IFormFile File { get; set; }
 
-        [JsonProperty("categoryId")]
         public int CategoryId { get; set; }
     }
 }
