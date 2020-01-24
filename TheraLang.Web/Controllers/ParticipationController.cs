cc4cde0b03c600ece4a1af36f47449cf288cba5a
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-01-24 23:19:18 +0200

    Refactoring ParticipationController

--- TheraLang.Web/Controllers/ParticipationController.cs
+++ TheraLang.Web/Controllers/ParticipationController.cs
@@ -16,14 +16,16 @@ namespace TheraLang.Web.Controllers
     [ApiController]
     public class ParticipationController : ControllerBase
     {
-        public ParticipationController(IProjectParticipationService service, UserManager<User> userManager)
+        public ParticipationController(IProjectParticipationService projectParticipationServiceservice, UserManager<User> userManager, IProjectService projectService)
         {
-            _service = service;
+            _projectParticipationServiceservice = projectParticipationServiceservice;
             _userManager = userManager;
+            _projectService = projectService;
         }
 
-        private readonly IProjectParticipationService _service;
+        private readonly IProjectParticipationService _projectParticipationServiceservice;
         private readonly UserManager<User> _userManager;
+        private readonly IProjectService _projectService;
 
         /// <summary>
         /// Change status of participant
@@ -43,7 +45,7 @@ namespace TheraLang.Web.Controllers
             var mapper = new MapperConfiguration(cfg => cfg.CreateMap<ProjectParticipationStatusViewModel, ProjectParticipationStatusDto>()).CreateMapper();
             var statusDto = mapper.Map<ProjectParticipationStatusViewModel, ProjectParticipationStatusDto>(status);
 
-            await _service.ChangeStatusAsync(participantId, statusDto);
+            await _projectParticipationServiceservice.ChangeStatusAsync(participantId, statusDto);
             return Ok();
         }
 
@@ -54,17 +56,9 @@ namespace TheraLang.Web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<ParticipantViewModel>> GetAllParticipants()
         {
-            var members = _service.GetAll().ToList();
+            var members = _projectParticipationServiceservice.GetAll().ToList();
 
-            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<ProjectParticipationDto, ParticipantViewModel>()
-                .ForMember(m => m.Id, opt => opt.MapFrom(m => m.PiranhaUser.Id))
-                .ForMember(m => m.UserName, opt => opt.MapFrom(m => m.PiranhaUser.UserName))
-                .ForMember(m => m.UserEmail, opt => opt.MapFrom(m => m.PiranhaUser.Email))
-            ).CreateMapper();
-
-            var membersModel = mapper.Map<IEnumerable<ProjectParticipationDto>, IEnumerable<ParticipantViewModel>>(members);
-
-            return Ok(membersModel);
+            return Ok(members);
         }
 
         /// <summary>
@@ -76,13 +70,16 @@ namespace TheraLang.Web.Controllers
         [Route("create")]
         public async Task<IActionResult> CreateParticipant([FromBody]int projectId)
         {
-            if (projectId == default)
+            var project = _projectService.GetById(projectId);
+
+            if (project == null)
             {
-                throw new ArgumentException($"The {nameof(projectId)} can not be 0");
+                NotFound();
             }
 
             User user = await _userManager.FindByNameAsync(User.Identity.Name);
-            await _service.CreateRequest(user.Id, projectId);
+            await _projectParticipationServiceservice.CreateRequest(user.Id, projectId);
+
             return Ok();
         }
     }
