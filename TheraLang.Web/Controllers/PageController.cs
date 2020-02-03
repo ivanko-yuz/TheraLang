e1ee696607f9a756291d631486aa4df6b65d8d82
Artur Britvin <artur.britvin@gmail.com>
2020-02-03 17:58:59 +0200

    Created view model for page

---
+++ TheraLang.Web/Controllers/PageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheraLang.BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace TheraLang.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PageController : ControllerBase
+    {
+        private readonly IPageService _pageService;
+        private readonly IUserManagementService _userManagementService;
+
+        public PageController(IPageService pageService, IUserManagementService userManagementService)
+        {
+            _pageService = pageService;
+            _userManagementService = userManagementService;
+        }
+
+
+    }
+}
