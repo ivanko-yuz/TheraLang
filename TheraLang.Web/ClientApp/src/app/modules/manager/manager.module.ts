6bd3214b9f44677409ba0d6f064d22d61c226b15
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-02-03 13:12:21 +0200

    config editor

--- TheraLang.Web/ClientApp/src/app/modules/manager/manager.module.ts
+++ TheraLang.Web/ClientApp/src/app/modules/manager/manager.module.ts
@@ -7,14 +7,27 @@ import { CreatePageComponent } from "./page-manager/create-page/create-page.comp
 import { ManagerComponent } from "./manager.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { QuillModule } from "ngx-quill";
+import { MatButtonModule, MatFormFieldModule, MatRippleModule, MatInputModule, MatCardModule } from '@angular/material';
+import { EditorComponent } from './shared/components/editor/editor.component';
 
 @NgModule({
-  declarations: [PageManagerComponent, CreatePageComponent, ManagerComponent],
+  declarations: [PageManagerComponent, CreatePageComponent, ManagerComponent, EditorComponent],
   imports: [
     CommonModule,
     ManagerRoutingModule,
     ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCardModule,
     QuillModule.forRoot()
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule
   ]
 })
 export class ManagerModule {}
--- TheraLang.Web/ClientApp/src/app/modules/manager/page-manager/create-page/create-page.component.ts
+++ TheraLang.Web/ClientApp/src/app/modules/manager/page-manager/create-page/create-page.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import { PageService } from 'src/app/core/http/page/page.service';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { PageService } from 'src/app/modules/manager/shared/services/page.service';
 import { Newpage } from 'src/app/shared/models/new_page/newpage';
+import { Location } from '@angular/common';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-create-page',
@@ -12,24 +14,31 @@ export class CreatePageComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private pageService: PageService) {
+  constructor(private pageService: PageService, private location: Location) {
   }
 
   ngOnInit() {
     this.form = new FormGroup({
-      title: new FormControl(null, Validators.required),
-      text: new FormControl(null, Validators.required),
-      author: new FormControl(null, Validators.required)
+      title: new FormControl(null, [Validators.required, Validators.maxLength(60)]),
+      text: new FormControl(null, [Validators.required, Validators.maxLength(5000)])
     })
+  }
+
+  public onCancel = () => {
+    this.location.back();
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.form.controls[controlName].hasError(errorName);
   }
 
   submit() {
     const page: Newpage = {
       title: this.form.value.title,
-      text: this.form.value.text,
-      date: new Date()
+      text: this.form.value.text
     }
 
     console.log(page)
+    this.pageService.addPage(page);
   }
 }
