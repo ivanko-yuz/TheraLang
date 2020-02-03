658368503456dd2f6f908ded8c28a0522b4e9d16
Artur Britvin <artur.britvin@gmail.com>
2020-02-03 17:29:32 +0200

    Added PageService

--- TheraLang.BLL/DataTransferObjects/PageDto.cs
+++ TheraLang.BLL/DataTransferObjects/PageDto.cs
@@ -8,7 +8,7 @@ namespace TheraLang.BLL.DataTransferObjects
     {
         public int Id { get; set; }
         public string Content { get; set; }
-        public string Title { get; set; }
+        public string Header { get; set; }
         public string MenuName { get; set; }
         public int? ParentPageId { get; set; }
 
--- TheraLang.DAL/Entities/Page.cs
+++ TheraLang.DAL/Entities/Page.cs
@@ -5,10 +5,10 @@ namespace TheraLang.DAL.Entities
     public class Page : BaseEntity
     {
         public string Content { get; set; }
-        public string Title { get; set; }
+        public string Header { get; set; }
         public string MenuName { get; set; }
         public int? ParentPageId { get; set; }
-        
+        public string Route { get; set; }
         public virtual Page ParentPage { get; set; }
         public virtual ICollection<Page> SubPages { get; set; }
     }
