143de6d6e27407d02598e1c68953f2d30f03c0bd
Artur <32452845+artur-britvin@users.noreply.github.com>
2020-02-03 18:02:53 +0200

    Feature/page editor backend (#167)
    * Added dto model
    
    * Added PageService
    
    * Created view model for page

--- TheraLang.DAL/Configuration/PageConfiguration.cs
+++ TheraLang.DAL/Configuration/PageConfiguration.cs
@@ -9,9 +9,16 @@ namespace TheraLang.DAL.Configuration
         public void Configure(EntityTypeBuilder<Page> builder)
         {
             builder.ToTable("Pages");
-            builder.Property(p => p.Title).IsRequired().HasMaxLength(256);
+            builder.Property(p => p.Header).IsRequired().HasMaxLength(128);
             builder.Property(p => p.Content).HasMaxLength(5000);
             builder.Property(p => p.MenuName).HasMaxLength(32);
+            builder.Property(p => p.Route).IsRequired().HasMaxLength(32);
+            builder.HasIndex(p => p.Route).IsUnique();
+
+            builder.HasMany(p => p.SubPages)
+                .WithOne(p => p.ParentPage)
+                .HasForeignKey(p => p.ParentPageId);
+       
         }
     }
 }
