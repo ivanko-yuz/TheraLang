320b7f983273eb6f336d95917c44aa4b368ad5fb
Volodymyr Krupskyi <krupskyi.volodymyr@gmail.com>
2020-02-06 12:25:30 +0200

    File service modification (#176)
    * Removed IFormFile from service layer
    
    * Cleaned up usings

--- TheraLang.BLL/Services/ResourceService.cs
+++ TheraLang.BLL/Services/ResourceService.cs
@@ -45,7 +45,9 @@ namespace TheraLang.BLL.Services
             {
                 if (resourceDto.File != null)
                 {
-                    var fileUri = await _fileService.SaveFile(resourceDto.File);
+                    var fileStream = resourceDto.File.OpenReadStream();
+                    var fileExtension = Path.GetExtension(resourceDto.File.FileName);
+                    var fileUri = await _fileService.SaveFile(fileStream,fileExtension);
                     resourceDto.Url = fileUri.ToString();
                 }
                 var mapper = new MapperConfiguration(cfg => cfg.CreateMap<ResourceDto, Resource>()
