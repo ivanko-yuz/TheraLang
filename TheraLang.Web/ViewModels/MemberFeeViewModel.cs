aea0a24c0ffc4ecb9e4e13800dc5925014373f69
Bohdan Sydor <bohdansydor2015@gmail.com>
2020-02-11 17:48:06 +0200

    add MemberFeeViewModel and MemberFeeDto

---
+++ TheraLang.Web/ViewModels/MemberFeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheraLang.Web.ViewModels
+{
+    public class MemberFeeViewModel
+    {
+        public DateTime FeeDate { get; set; }
+        public decimal FeeAmount { get; set; }
+
+    }
+}
