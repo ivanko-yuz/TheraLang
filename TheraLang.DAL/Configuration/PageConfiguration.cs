5ed3285f826bc45bf55155275ce0bf19a9a23401
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-02-12 21:13:39 +0200

    Changed page entity, dto, view models

--- TheraLang.DAL/Configuration/PageConfiguration.cs
+++ TheraLang.DAL/Configuration/PageConfiguration.cs
@@ -10,10 +10,13 @@ namespace TheraLang.DAL.Configuration
         {
             builder.ToTable("Pages");
             builder.Property(p => p.Header).IsRequired().HasMaxLength(128);
-            builder.Property(p => p.Content).HasMaxLength(5000);
+            builder.Property(p => p.Content).HasMaxLength(8000);
             builder.Property(p => p.MenuName).HasMaxLength(32);
-            builder.Property(p => p.Route).IsRequired().HasMaxLength(32);
             builder.HasIndex(p => p.Route).IsUnique();
+            builder.Property(e => e.Route)
+                .IsRequired()
+                .HasMaxLength(256);
+
             builder.HasMany(p => p.SubPages)
                 .WithOne(p => p.ParentPage)
                 .HasForeignKey(p => p.ParentPageId);
