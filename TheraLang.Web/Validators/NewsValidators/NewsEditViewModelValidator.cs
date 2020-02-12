a40f6b5e1839c512558fccf39ae32b18bfdf5454
Dreylak <andreylakusta@gmail.com>
2020-02-12 15:05:45 +0200

    Added todo comment

--- TheraLang.Web/Validators/NewsValidators/NewsEditViewModelValidator.cs
+++ TheraLang.Web/Validators/NewsValidators/NewsEditViewModelValidator.cs
@@ -11,6 +11,7 @@ namespace TheraLang.Web.NewsValidators.Validators
         {
             RuleFor(x => x.Title).NotNull().NotEmpty().MinimumLength(3).MaximumLength(250);
             RuleFor(x => x.Text).NotNull().NotEmpty().MinimumLength(5).MaximumLength(10000);
+            //TODO: Check
             RuleFor(x => x.NewMainImage).IsImage().NotNull().NotEmpty().When(model => model.UploadedMainImageUrl == null);
             RuleFor(x => x.UploadedMainImageUrl).NotNull().NotEmpty().When(model => model.NewMainImage == null);
             RuleFor(x => x.AddedContentImages).ForEach(img => img.IsImage());
