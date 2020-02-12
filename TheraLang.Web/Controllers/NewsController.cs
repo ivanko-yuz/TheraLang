7f1fd8769fa124b65352647b2ffbaa9e075ce5ef
Dreylak <andreylakusta@gmail.com>
2020-02-12 18:10:31 +0200

    Cleaned up code

--- TheraLang.Web/Controllers/NewsController.cs
+++ TheraLang.Web/Controllers/NewsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TheraLang.BLL.DataTransferObjects;
