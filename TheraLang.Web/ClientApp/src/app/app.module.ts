6b12ae7d9c776e6fee57fb29acbc2f1e1d19bd3d
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-02-13 19:36:08 +0200

    LVIIICNET-149 Add not found page (#184)
    * Add not found page
    
    * Little fix

--- TheraLang.Web/ClientApp/src/app/app.module.ts
+++ TheraLang.Web/ClientApp/src/app/app.module.ts
@@ -53,7 +53,6 @@ import { CdkTreeModule } from "@angular/cdk/tree";
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import { DragDropModule } from "@angular/cdk/drag-drop";
 import { ConfirmDialogComponent } from "./shared/components/confirm-dialog/confirm-dialog.component";
-import { ErrorComponent } from "./shared/components/error/error.component";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { ToolbarComponent } from "./modules/main/toolbar/toolbar.component";
@@ -98,6 +97,7 @@ import { MaterialFileInputModule } from "ngx-material-file-input";
 import { SortablejsModule } from "ngx-sortablejs";
 import { JwtModule } from "@auth0/angular-jwt";
 import { DaysLeftPipe } from "./modules/main/pages/project/days-left.pipe";
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { AuthGuard } from "./shared/guards/auth-guard.service";
 import { AdminGuard } from "./shared/guards/admin-guard.service";
 
@@ -121,7 +121,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     CustomDatePipe,
     ResourcesTableComponent,
     ConfirmDialogComponent,
-    ErrorComponent,
     ProjectFormComponent,
     ResourcesInternalTableComponent,
     GeneralResourcesTableComponent,
@@ -138,7 +137,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     LanguageComponent,
     MainComponent,
     CmsPagesToolbarItemComponent,
-    DaysLeftPipe
+    DaysLeftPipe,
+    NotFoundComponent
   ],
   entryComponents: [
     ResourcesInternalTableComponent,
