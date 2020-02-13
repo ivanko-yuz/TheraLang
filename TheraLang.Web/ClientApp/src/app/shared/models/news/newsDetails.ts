4b9442e708d7fb37f521ddbadcdb5af0191c4ad1
Dmytro Rudevych <mcatomicamc@gmail.com>
2020-02-13 16:16:24 +0200

    Created entities for news and news details

---
+++ TheraLang.Web/ClientApp/src/app/shared/models/news/newsDetails.ts
@@ -0,0 +1,9 @@
+export class NewsDetails {
+    id: number
+    title: string
+    text: string
+    mainImageUrl : string
+    contentImageUrls: string[]
+    authorName: string
+    createdDateUtc : Date
+}
