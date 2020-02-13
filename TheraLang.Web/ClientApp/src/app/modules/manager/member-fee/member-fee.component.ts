752580e8809665c1c5ba137afe10675b325a10cc
Bohdan Sydor <bohdansydor2015@gmail.com>
2020-02-13 15:46:56 +0200

    add member-fee component

---
+++ TheraLang.Web/ClientApp/src/app/modules/manager/member-fee/member-fee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-fee',
+  templateUrl: './member-fee.component.html',
+  styleUrls: ['./member-fee.component.less']
+})
+export class MemberFeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
