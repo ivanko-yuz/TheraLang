30b03d97536c549d4c034a89e602f29aef7b8799
Dmytro Rudevych <mcatomicamc@gmail.com>
2020-02-16 18:22:31 +0200

    Added news create page

--- TheraLang.Web/ClientApp/src/app/core/http/news/news.service.ts
+++ TheraLang.Web/ClientApp/src/app/core/http/news/news.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { baseUrl } from 'src/app/configs/api-endpoint.constants';
+import { NewsCreate } from 'src/app/shared/models/news/newsCreate';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,8 @@ export class NewsService {
     return this.http.get(this.url+"news/"+id);
   }
 
-  
+  createNews(news){
+    return this.http.post(this.url + "news",news);
+  }
 
 }
--- TheraLang.Web/ClientApp/src/app/modules/main/pages/news/news-create/news-create.component.ts
+++ TheraLang.Web/ClientApp/src/app/modules/main/pages/news/news-create/news-create.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NewsService } from 'src/app/core/http/news/news.service';
+import { Router } from '@angular/router';
+import { NewsCreate } from 'src/app/shared/models/news/newsCreate';
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
+import { Resource } from 'src/app/shared/models/resource/resource';
 
 @Component({
   selector: 'app-news-create',
@@ -10,16 +16,63 @@ export class NewsCreateComponent implements OnInit {
 
   public newsForm: FormGroup;
 
-  constructor() { 
+  constructor(
+    private service:NewsService,
+    private router: Router,
+    private translate: TranslateService, 
+    private notificationService: NotificationService
+    ) 
+    { 
     this.newsForm = new FormGroup({
-      "title":new FormControl("",[Validators.maxLength(250), Validators.minLength(3)]),
-      "text":new FormControl("", [Validators.maxLength(10000), Validators.minLength(5)]),
-      "previewImage":new FormControl(null),
-      "contentImages":new FormControl()
+      "title":new FormControl("",[Validators.required,Validators.maxLength(250), Validators.minLength(3)]),
+      "text":new FormControl("", [Validators.required,Validators.maxLength(10000), Validators.minLength(5)]),
+      "previewImage":new FormControl(null,[Validators.required,Validators.max(1)]),
+      "contentImages":new FormControl(null)
     });
   }
 
   ngOnInit() {
+  }
+
+  initializeForm() {
+    this.newsForm.setValue({
+      title: "",
+      text: "",
+      previewImage: null,
+      contentImages: null
+    });
+  }
+
+  onSubmit() {
+    if (this.newsForm.invalid) {
+      const controls = this.newsForm.controls;
+      Object.keys(controls).forEach(controlName =>
+        controls[controlName].markAsTouched()
+      );
+      return;
+    } 
+    else {
+      const formData = new FormData();
+      const news = this.newsForm.value as NewsCreate
+        formData.append("title",news.title);
+        formData.append("text", news.text);
+        formData.append("previewImage", news.mainImageUrl);
+        formData.append("contentImages", JSON.stringify(news.contentImageUrls));
+      this.service.createNews(formData).subscribe(
+        async (msg: string) => {
+          msg = await this.translate
+            .get("common.created-successfully")
+            .toPromise();
+          this.notificationService.success(msg);
+        },
+        async error => {
+          console.log(error);
+          this.notificationService.warn(
+            await this.translate.get("common.wth").toPromise()
+          );
+        })
+      this.router.navigate(["/news/create"]); 
+    }
   }
 
 }
