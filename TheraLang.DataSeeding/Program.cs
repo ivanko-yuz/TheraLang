9a6dcb06dfaa6650023e5a098945be5c6ec20db7
Volodymyr Krupskyi <krupskyi.volodymyr@gmail.com>
2020-02-20 17:54:49 +0200

    Moved dataseeding to separate runnable project

--- TheraLang.DataSeeding/Program.cs
+++ TheraLang.DataSeeding/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Common.Helpers.PasswordHelper;
 using TheraLang.DAL;
 using TheraLang.DAL.Entities;
 
@@ -10,35 +12,42 @@ namespace TheraLang.DataSeeding
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             using var dbContext = CreateDbContext();
+            var adminRoleId = new Guid("f4cbff0f-4bc0-42a4-9738-8d9f9bb734ba");
+            var memberRoleId = new Guid("50245ab3-6770-4269-ac26-30a942116a70");
+            var guestRoleId = new Guid("7e9b3674-e720-4d50-939b-93ce8e8b1c44");
+            
+            var adminId = new Guid("f5aa0c0e-b669-466c-825a-2c52c58a019b");
+            var memberId = new Guid("3bcec608-e6a4-45e5-b3b2-cdf252a639de");
+            var guestId = new Guid("29cf2d1c-6328-4861-a718-6a6bcc984337");
 
-            var projectTypes = ProjectTypes().ToArray();
-            dbContext.ClearAndSeed(projectTypes);
-            dbContext.ClearAndSeed(Projects(projectTypes));
-        }
+            var roles = new List<Role>()
+            {
+                new Role() {Id = adminRoleId, Name = "Admin"},
+                new Role() {Id = memberRoleId, Name = "Member"},
+                new Role() {Id = guestRoleId, Name = "Guest"}
+            };
+            
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    Id = adminId, Email = "admin@utmm.com",
+                    PasswordHash = PasswordHasher.HashPassword("password"), RoleId = adminRoleId
+                },
+                new User
+                {
+                    Id = memberId, Email = "member@utmm.com",
+                    PasswordHash = PasswordHasher.HashPassword("password"), RoleId = memberRoleId
+                }
+            };
 
-        private static IEnumerable<Project> Projects(ProjectType[] projectTypes)
-        {
-            yield return new Project
-            {
-                Name = "ProjectName",
-                Description = "ProjectDescription",
-                Details = "ProjectDetails",
-                ProjectStart = new DateTime(2000, 8, 30),
-                ProjectEnd = new DateTime(2019, 8, 30),
-                IsActive = true,
-                TypeId = projectTypes[0].Id,
-            };
+            await dbContext.ClearAndSeed(roles);
+            await dbContext.ClearAndSeed(users);
         }
-        private static IEnumerable<ProjectType> ProjectTypes()
-        {
-            yield return new ProjectType
-            {
-                TypeName = "name"
-            };
-        }
+        
         private static DbContext CreateDbContext()
         {
 
