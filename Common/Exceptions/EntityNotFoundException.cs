a3c37dee9d7788f560c4f3490134ced6afa4a020
Volodymyr Krupskyi <krupskyi.volodymyr@gmail.com>
2020-02-24 18:31:42 +0200

    Added custom exceptions

---
+++ Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Exceptions
+{
+    public class EntityNotFoundException : ApiException
+    {
+        public EntityNotFoundException(string message, params object[] args) : base(string.Format(message, args))
+        {
+        }
+
+        public EntityNotFoundException(string entityName, object entityId) : base("{0} with id {1} not found",
+            entityName, entityId)
+        {
+        }
+    }
+}
