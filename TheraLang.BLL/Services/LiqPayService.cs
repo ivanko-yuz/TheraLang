752f489263b77e89474748504ba05b47bda9d198
Volodymyr Krupskyi <krupskyi.volodymyr@gmail.com>
2020-02-24 18:32:35 +0200

    Created enums required to process payment

---
+++ TheraLang.BLL/Services/LiqPayService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using AutoMapper;
+using Common.Exceptions;
+using TheraLang.BLL.DataTransferObjects.Donations;
+using TheraLang.BLL.DataTransferObjects.LiqPay;
+using TheraLang.BLL.Interfaces;
+using TheraLang.BLL.LiqPay;
+
+namespace TheraLang.BLL.Services
+{
+    public class LiqPayService: ILiqPayService
+    {
+        public async Task<LiqPayCheckoutDto> GetLiqPayCheckoutModelAsync(
+            LiqPayCheckoutModelRequestDto liqPayCheckoutModelRequest, string returnUrl, string callbackServerUrl)
+        {
+            _ = returnUrl ?? throw new InvalidArgumentException(nameof(returnUrl), "cannot be null");
+            _ = callbackServerUrl ?? throw new InvalidArgumentException(nameof(callbackServerUrl), "cannot be null"); 
+            
+            var mapper = new MapperConfiguration(mapOpts =>
+            {
+                mapOpts.CreateMap<LiqPayCheckoutModelRequestDto, LiqPayCheckout>()
+                    .ForMember(c => c.Amount,
+                        opts =>
+                            opts.MapFrom(dto => dto.DonationAmount))
+                    .ForMember(c => c.ResultUrl,
+                        opts =>
+                            opts.MapFrom(dto => returnUrl))
+                    .ForMember(c => c.ServerUrl,
+                        opts =>
+                            opts.MapFrom(dto => callbackServerUrl));
+            }).CreateMapper();
+
+            var liqPayCheckout = mapper.Map<LiqPayCheckout>(liqPayCheckoutModelRequest);
+            var liqPayData = new LiqPayData(liqPayCheckout);
+            var liqPaySignature = new LiqPaySignature(liqPayData);
+            
+            return new LiqPayCheckoutDto()
+            {
+                Data = liqPayData.Base64Data,
+                Signature = await liqPaySignature.GetSignature()
+            };
+        }
+    }
+}
