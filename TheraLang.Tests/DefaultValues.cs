cb6bdf68ba650acb0651210c7e3e5d8303e6e13e
Dreylak <andreylakusta@gmail.com>
2020-03-07 13:20:48 +0200

    Added DataBuilder to tests and a few test methods

---
+++ TheraLang.Tests/DefaultValues.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheraLang.BLL.DataTransferObjects.CommentDtos;
+
+namespace TheraLang.Tests
+{
+    static class DefaultValues
+    {
+        public static readonly int ListSize = 10;
+        public static readonly int FakeId = -10;
+        public static readonly int ExistedId = 1;
+        public static readonly PaginationParams PaginationParams = new PaginationParams() { PageNumber = 1, PageSize = 2 };
+        public static readonly PaginationParams FakePaginationParams = new PaginationParams() { PageNumber = 999, PageSize = 999 };
+        public static readonly CommentRequestDto CommentRequest = new CommentRequestDto() { Text = "Text" };
+    }
+}
--- TheraLang.Tests/Services/NewsCommentServiceTests.cs
+++ TheraLang.Tests/Services/NewsCommentServiceTests.cs
@@ -1,18 +1,17 @@
 ﻿using Common.Exceptions;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
-using TheraLang.BLL.DataTransferObjects.CommentDtos;
 using TheraLang.BLL.Services;
 using TheraLang.DAL.Entities;
 using TheraLang.DAL.Repository;
 using TheraLang.DAL.UnitOfWork;
+using TheraLang.Tests.DataBuilders.ResoursesBuilders;
 using Xunit;
 
 namespace TheraLang.Tests.Services
@@ -23,17 +22,14 @@ namespace TheraLang.Tests.Services
         private readonly NewsCommentService _commentsService;
         private readonly List<NewsComment> _testDb;
 
-        private int notExistedId = -10;
-        private int existedId = 1;
-        private int newsId = 1;
-
         public NewsCommentServiceTests()
         {
             _testDb = GetTestData().ToList();
 
             var repoMock = new Mock<IRepository<NewsComment>>();
-            
-            repoMock.Setup(r=>r.Get(It.IsAny<Expression<Func<NewsComment, bool>>>()))
+
+            repoMock.Setup(r => r.GetAll()).Returns(_testDb.AsQueryable().BuildMock().Object);
+            repoMock.Setup(r => r.Get(It.IsAny<Expression<Func<NewsComment, bool>>>()))
                 .ReturnsAsync((Expression<Func<NewsComment, bool>> predicate) => _testDb
                     .AsQueryable()
                     .Where(predicate)
@@ -55,44 +51,78 @@ namespace TheraLang.Tests.Services
         [Fact]
         public async Task RemoveComments_NotFoundException()
         {
-            Func<Task> result = async() => await _commentsService.RemoveComment(notExistedId);
+            Func<Task> result = async () => await _commentsService.RemoveComment(DefaultValues.FakeId);
             await result.Should().ThrowAsync<NotFoundException>();
         }
 
         [Fact]
         public async Task RemoveComments_ShouldCallSaveChanges()
         {
-            await _commentsService.RemoveComment(existedId);
+            await _commentsService.RemoveComment(DefaultValues.ExistedId);
             _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
         }
 
         [Fact]
         public async Task UpdateComments_NotFoundException()
         {
-            Func<Task> result = async () => await _commentsService.UpdateComment(notExistedId, null);
+            Func<Task> result = async () => await _commentsService.UpdateComment(DefaultValues.FakeId, null);
             await result.Should().ThrowAsync<NotFoundException>();
         }
 
         [Fact]
         public async Task UpdateComments_ShouldCallSaveChanges()
         {
-            var comment = new CommentRequestDto() { Text = "New Text" };
-            await _commentsService.UpdateComment(existedId, comment);
+            await _commentsService.UpdateComment(DefaultValues.ExistedId, DefaultValues.CommentRequest);
             _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddComment_ShouldCallSaveChanges()
+        {
+            await _commentsService.AddComment(DefaultValues.CommentRequest);
+            _unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetCommentsForNewsCount_ShouldReturnCommentsCount()
+        {
+            int actualResult = _testDb.Where(c => c.NewsId == DefaultValues.ExistedId).Count();
+            int result = await _commentsService.GetCommentsForNewsCount(DefaultValues.ExistedId);
+
+            result.Should().Be(actualResult);
+        }
+
+        [Fact]
+        public async Task GetCommentsForNews_NotFoundException()
+        {
+            Func<Task> result = async () => await _commentsService.GetCommentsForNews(DefaultValues.FakeId);
+            await result.Should().ThrowAsync<NotFoundException>();
+        }
+
+        [Fact]
+        public async Task GetCommentsForNewsPage_NotFoundException()
+        {
+            Func<Task> result = async () => await _commentsService
+                .GetCommentsForNewsPage(DefaultValues.FakeId, DefaultValues.FakePaginationParams);
+            await result.Should().ThrowAsync<NotFoundException>();
         }
 
         private IEnumerable<NewsComment> GetTestData()
         {
-            return new List<NewsComment>()
+            var data = new List<NewsComment>();
+            
+            for (int i = 0; i < DefaultValues.ListSize; i++)
             {
-                new NewsComment()
-                {
-                    Id = 1,
-                    Text = "Text",
-                    CreatedById = new Guid(),
-                    NewsId = 1
-                }
-            };
+                var dataBuilder = new CommentsBuilder();
+                data.Add(dataBuilder
+                    .WithId(i)
+                    .WithAuthorId(new Guid())
+                    .WithNewsId(DefaultValues.ExistedId)
+                    .WithDefaultText()
+                    .Build()); 
+            }
+
+            return data.AsEnumerable();
         }
     }
 }
