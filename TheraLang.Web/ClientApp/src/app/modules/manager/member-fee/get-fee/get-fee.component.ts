f4044883980147b3213a4430933cc1a06d8096c0
Bohdan Sydor <bohdansydor2015@gmail.com>
2020-03-09 18:03:32 +0200

    add notification to delete fee

--- TheraLang.Web/ClientApp/src/app/modules/manager/member-fee/get-fee/get-fee.component.ts
+++ TheraLang.Web/ClientApp/src/app/modules/manager/member-fee/get-fee/get-fee.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MemberFee } from 'src/app/shared/models/member-fee/member-fee';
 import { MemberFeeService } from 'src/app/core/http/manager/fee.service';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { DialogService } from 'src/app/core/services/dialog/dialog.service';
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
 
 @Component({
   selector: 'app-get-fee',
@@ -13,9 +16,12 @@ export class GetFeeComponent implements OnInit {
   memberFee: MemberFee = new MemberFee();
   memberFees: MemberFee[] = [];
   constructor(
-    private router:Router,
-    private memberFeeService: MemberFeeService
-    ) { }
+    private router: Router,
+    private memberFeeService: MemberFeeService,
+    private dialogService: DialogService,
+    private translate: TranslateService,
+    private notificationService: NotificationService,
+  ) { }
 
   ngOnInit() {
     this.loadMemberFees();
@@ -24,12 +30,29 @@ export class GetFeeComponent implements OnInit {
   loadMemberFees() {
     this.memberFeeService.getMemberFees().subscribe({
       next: (data: MemberFee[]) => {
-         this.memberFees = data;
+        this.memberFees = data;
       }
     });
   }
-  delete(fee: MemberFee) {
-    this.memberFeeService.deleteMemberFee(fee.id)
-      .subscribe(data => this.loadMemberFees());
+  async onDelete(fee: MemberFee) {
+    this.dialogService
+      .openConfirmDialog(await this.translate.get("common.r-u-sure").toPromise())
+      .afterClosed()
+      .subscribe(async res => {
+        if (res) {
+            this.memberFeeService.deleteMemberFee(fee.id).subscribe(async result => {
+              this.loadMemberFees();
+            this.notificationService.success(await this.translate.get("common.deleted-successfully").toPromise());
+          });
+        }
+      },
+        async error => {
+          console.log(error);
+          error = await this.translate
+            .get("common.wth")
+            .toPromise();
+          this.notificationService.warn(error);
+        }
+      );
   }
 }
