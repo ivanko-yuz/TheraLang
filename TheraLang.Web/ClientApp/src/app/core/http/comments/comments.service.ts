d7795223042c828a6dac3fc8296e3c3744cfa323
Dreylak <andreylakusta@gmail.com>
2020-03-26 16:41:25 +0200

    Changed url concatecation with interpolation in comments service on frontend

--- TheraLang.Web/ClientApp/src/app/core/http/comments/comments.service.ts
+++ TheraLang.Web/ClientApp/src/app/core/http/comments/comments.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { baseUrl } from 'src/app/configs/api-endpoint.constants';
+import { commentUrl } from 'src/app/configs/api-endpoint.constants';
 import { HttpClient } from '@angular/common/http';
 import { PaginationParams } from 'src/app/shared/models/pagination-params/pagination-params';
 
@@ -8,32 +8,29 @@ import { PaginationParams } from 'src/app/shared/models/pagination-params/pagina
 })
 export class CommentsService {
 
-  private url = baseUrl;
-
   constructor(private http: HttpClient) { }
 
-  getCommentsCount(newsId: number){
-    return this.http.get(this.url + "comment/count/" + newsId);
+  getCommentsCount(newsId: number) {
+    return this.http.get(`${commentUrl}/count/${newsId}`);
   }
 
   getAllComments(newsId: number) {
-    return this.http.get(this.url + "comment/all/" + newsId);
+    return this.http.get(`${commentUrl}/all/${newsId}`);
   }
 
   getCommentsPage(newsId: number, paginationParams: PaginationParams) {
-    return this.http.get(this.url + "comment/" + newsId + "?pageNumber=" + paginationParams.pageNumber 
-      + "&pageSize=" + paginationParams.pageSize);
+    return this.http.get(`${commentUrl}/${newsId}?pageNumber="${paginationParams.pageNumber}&pageSize=${paginationParams.pageSize}`);
   }
 
   createComment(comment) {
-    return this.http.post(this.url + "comment", comment);
+    return this.http.post(commentUrl, comment);
   }
 
   deleteComment(id: number) {
-    return this.http.delete(this.url + "comment/" + id);
+    return this.http.delete(`${commentUrl}/${id}`);
   }
 
   editComment(id: number, editedComment) {
-    return this.http.put(this.url + "comment/" + id, editedComment);
+    return this.http.put(`${commentUrl}/${id}`, editedComment);
   }
 }
