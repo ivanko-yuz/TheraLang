6cc966c6b4e8a8960d1a1ff47e00f4eef18578f3
Andriana <andrianabaran17@gmail.com>
2020-05-29 12:36:51 +0300

    redisign user list

--- TheraLang.Web/ClientApp/src/app/app.module.ts
+++ TheraLang.Web/ClientApp/src/app/app.module.ts
@@ -134,8 +134,7 @@ import {ResourcesTableComponent} from "./modules/main/pages/resource/resources-t
 import {ConfirmationComponent} from './modules/registration/confirmation/confirmation.component';
 import {ForgotPasswordComponent} from './modules/password/email/forgot-password.component';
 import {ResetPasswordComponent} from './modules/password/reset-password.component';
-import { NgxFlagIconCssModule } from 'ngx-flag-icon-css'
-
+import { NgxFlagIconCssModule } from 'ngx-flag-icon-css';
 export function tokenGetter() {
   return localStorage.getItem("jwt");
 }
